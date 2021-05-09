6c684aa096885af5b600b604b4b0d6fd133b0ac0
gaowen111 <81388331+gaowen111@users.noreply.github.com>
2021-05-09 21:25:29 +0200

    Add files via upload

---
+++ Capstone_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook is for IBM data science captone project
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
